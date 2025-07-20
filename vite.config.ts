b684fdc140c632e7a2660f1695e025afd784baf5
Dave Klee <dgklee@gmail.com>
2025-07-20 11:59:50 -0400

    Update for github pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/calorieclimb.com/', // <-- Set base for GitHub Pages
   plugins: [react()],
-  base: '/calorieclimb.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
